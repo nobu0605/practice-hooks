9c4a73d3393d4e2160cfeafe2a10346f655a57fe
鈴木孝伸 <>
2021-02-23 22:37:01 +0900

    practice-hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createContext, useContext } from 'react';
+const MyContext = createContext();
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  function Child1() {
+    return <Child3 />;
+  }
+
+  function Child2() {
+    return <h2>Not Consumer</h2>;
+  }
+
+  function Child3() {
+    const name = useContext(MyContext);
+
+    return <h1>{name}</h1>;
+  }
+
+  useEffect(() => {
+    document.title = `${count}回クリックされました`;
+    setNumber(number + 1);
+  }, [count]);
+  const name = 'Consumer';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{document.title}</span>
+      {number}
+      <button onClick={() => setCount((prev) => prev + 1)}>+ Add</button>
+      <MyContext.Provider value={name}>
+        <Child1 />
+        <Child2 />
+      </MyContext.Provider>
     </div>
   );
 }
